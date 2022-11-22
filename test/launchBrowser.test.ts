fdc005a6d2d27a362869cdaca4a352eac708bb4f
juliendy <julien.yasar@gmail.com>
2022-11-22 07:04:05 +0100

    feat: record video define in context

--- test/launchBrowser.test.ts
+++ test/launchBrowser.test.ts
@@ -5,7 +5,11 @@ const baseURL = "https://the-internet.herokuapp.com/";
 describe("Launch Browser", () => {
     test("Open web page", async () => {
         const browser = await chromium.launch({ headless: false });
-        const context = await browser.newContext();
+        const context = await browser.newContext({
+            recordVideo: {
+                dir: "videos/",
+            },
+        });
         const page = await context.newPage();
         await page.goto(baseURL);
 

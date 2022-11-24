a1140fc3fcb66af993b4cab644a65dad614c6613
juliendy <julien.yasar@gmail.com>
2022-11-24 01:01:41 +0100

    chore: fixes pages file names

--- page/loginPage.ts
+++ page/loginPage.ts
@@ -1,26 +1,23 @@
 import { Page } from "playwright";
-
 export default class LoginPage {
     private page: Page;
     constructor(page: Page) {
         this.page = page;
     }
-
     public get elementEmailTextField() {
         return this.page.locator("input[id='userName']");
     }
-
     // short way of writing same thing:
     elementPasswordTextField = async () =>
         this.page.locator("input[id='password']");
-
     public get elementLoginBtn() {
         return this.page.locator("button[id='login']");
     }
 
     public async enterUserName(username: string) {
         const elem = this.elementEmailTextField;
-        await elem?.fill(username);
+        if (elem != null) await elem.fill(username);
+        else throw new Error("Element input field for username is not found");
     }
 
     public async enterPassword(password: string) {
@@ -28,18 +25,15 @@ export default class LoginPage {
         const elem = await this.elementPasswordTextField();
         await elem?.fill(password);
     }
-
     public async clickLoginBtn() {
         const elem = this.elementLoginBtn;
         await elem?.click();
     }
-
     public async login_form(username: string, password: string) {
         await this.enterUserName(username);
         await this.enterPassword(password);
         await this.clickLoginBtn();
     }
-
     public get errorLoginMsg() {
         return this.page.locator("//p[@id='name']");
     }

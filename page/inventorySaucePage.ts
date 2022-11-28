2ed2c0183f8a496888f6a6a59d0184038505a398
juliendy <julien.yasar@gmail.com>
2022-11-28 06:57:56 +0100

    feat: test file inventory page + remove consent acceptance

--- page/inventorySaucePage.ts
+++ page/inventorySaucePage.ts
@@ -4,6 +4,9 @@ import BasePage from "./baseSaucePage";
 export default class InventoryPage extends BasePage {
     readonly burgerMenuId = "button[id='react-burger-menu-btn']";
     readonly logoutId = "#logout_sidebar_link";
+    readonly inventoryListClass = ".inventory_list";
+    readonly selectSortClass = ".product_sort_container";
+    readonly shoppingCartClass = ".shopping_cart_link";
 
     constructor(page: Page) {
         super(page);
@@ -35,5 +38,17 @@ export default class InventoryPage extends BasePage {
         expect(await this.burgerMenu.isVisible()).toBe(true);
         await this.clickBurgerMenu();
         await this.clickLogoutLink();
+    }
+    
+    public get productsList() {
+        return this.page.locator(this.inventoryListClass);
+    }
+
+    public get selectSortFilter() {
+        return this.page.locator(this.selectSortClass);
+    }
+
+    public get shoppingCart() {
+        return this.page.locator(this.shoppingCartClass);
     }
 }
